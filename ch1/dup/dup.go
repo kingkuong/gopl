55ab80001a8db00b41dffd6ba47d3c510a51af67
Cuong Tran <cuong@koho.ca>
2025-09-04 17:36:24 -0400

    Code for chapter 1

---
+++ ch1/dup/dup.go
@@ -0,0 +1,23 @@
+// dup prints the count and text of lines that apper more than once
+// in the input. It reads from stdin
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
